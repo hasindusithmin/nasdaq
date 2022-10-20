503ae3c4411ffdadb4df831e3eddc482afb9fef2
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-20 07:07:43 +0000

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,431 @@
+import Script from 'next/script'
+
+
+export default function Navbar() {
+    const wrapperStyle = {
+        width: "100%",
+        height: "100%"
+    }
+    return <>
+        <div id="tradingview-wrapper" style={wrapperStyle}>
+            <Script
+                id="tradingview-widget"
+                src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"
+                onLoad={() => {
+                    document.getElementById('tradingview-wrapper').appendChild(document.getElementById('tradingview-widget'));
+                }}>
+                {JSON.stringify({
+                    "symbols": [
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AAPL"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ABNB"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ADBE"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ADI"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ADP"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ADSK"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AEP"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ALGN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AMAT"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AMD"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AMGN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AMZN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ANSS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ASML"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ATVI"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AVGO"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:AZNA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:BIDU"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:BIIB"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:BKNG"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CDNS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CEG"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CHTR"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CMCSA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:COST"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CPRT"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CRWD"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CSCO"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CSX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CTAS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:CTSH"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:DDOG"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:DLTR"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:DOCU"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:DXCM"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:EA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:EBAY"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:EXC"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:FAST"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:FISV"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:FTNT"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:GILD"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:GOOG"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:GOOGL"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:HON"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:IDXX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ILMN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:INTC"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:INTU"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ISRG"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:JD"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:KDP"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:KHC"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:KLAC"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:LCID"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:LRCX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:LULU"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MAR"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MCHP"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MDLZ"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MELI"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:META"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MNST"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MRNA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MRVL"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MSFT"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MTCH"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:MU"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:NFLX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:NTES"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:NVDA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:NXPI"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ODFL"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:OKTA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ORLYO"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PANW"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PAYX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PCAR"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PDD"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PEP"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:PYPL"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:QCOM"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:REGN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:ROST"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SBUX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SGEN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SIRI"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SNPS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SPLK"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:SWKS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:TEAM "
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:TMUS"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:TSLA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:TXN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:VRSK"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:VRSN"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:VRTX"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:WBA"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:WDAY"
+                        },
+                        {
+                            "description": "",
+                            "proName": "NASDAQ:XEL"
+                        }
+                    ],
+                    "showSymbolLogo": true,
+                    "colorTheme": "light",
+                    "isTransparent": false,
+                    "displayMode": "adaptive",
+                    "locale": "en"
+                }
+                )}
+            </Script>
+        </div>
+    </>
+}
+
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import '../styles/w3.css'
 import '../styles/oswald.css'
 import '../styles/open-sans.css'
 import '../styles/globals.css'
-
+import Navbar from '../components/Navbar'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

3eaedfb472d92bde9b5c1595e5ce577dd08073ef
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-20 18:41:59 +0000

    Update next_config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
+
 const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
   pwa:{
     dest:"public",
     register:true,
@@ -10,4 +10,5 @@ const nextConfig = withPWA({
     disable: process.env.NODE_ENV === 'development'
   }
 })
+
 module.exports = nextConfig
